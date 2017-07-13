36c43fd8fe325334358d160cef71554d4292facd
Nate Hicks <nhicksdesign@gmail.com>
2017-07-12 20:00:46 -0400

    BIG UPDATE
    SVG updates, addings queries,

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,40 +1,76 @@
-window.onscroll = function () {
-    scrollFunction()
-};
+$(document).ready(function () {
 
-function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
+    /*STICKY NAVIGATION*/
+    $("#about").waypoint(function (direction) {
+        if (direction == 'down') {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
-    document.documentElement.scrollTop = 0; // For IE and Firefox
-}
+    /*SCROLL TO*/
+    $('.home-button').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.home-image').offset().top
+        }, 1000);
+    });
 
-var $contactForm = $('#contact-form');
-$contactForm.submit(function (e) {
-    e.preventDefault();
-    $.ajax({
-        url: '//formspree.io/megan.gerhard326@gmail.com',
-        method: 'POST',
-        data: $(this).serialize(),
-        dataType: 'json',
-        beforeSend: function () {
-            $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
-        },
-        success: function (data) {
-            $contactForm.find('.alert--loading').hide();
-            $contactForm.append('<div class="alert alert--success">Message sent!</div>');
-        },
-        error: function (err) {
-            $contactForm.find('.alert--loading').hide();
-            $contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+    $('.about-button').click(function () {
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 1000);
+    });
+
+    $('.calendar-button').click(function () {
+        $('html, body').animate({
+            scrollTop: $("#calendar").offset().top
+        }, 1000);
+    });
+
+    $('.contact-button').click(function () {
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top
+        }, 1000);
+    });
+
+    /*MOBILE NAVIGATION*/
+    $('.mobile-nav-icon').click(function () {
+        var nav = $('.main-nav');
+        var icon = $('.mobile-nav-icon i');
+
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion.close.round');
         }
     });
-});
 
+    /*CONTACT FORM*/
+    var $contactForm = $('#contact-form');
+    $contactForm.submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: '//formspree.io/megan.gerhard326@gmail.com',
+            method: 'POST',
+            data: $(this).serialize(),
+            dataType: 'json',
+            beforeSend: function () {
+                $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+            },
+            success: function (data) {
+                $contactForm.find('.alert--loading').hide();
+                $contactForm.append('<div class="alert alert--success">Message sent!</div>');
+            },
+            error: function (err) {
+                $contactForm.find('.alert--loading').hide();
+                $contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+            }
+        });
+    });
+});
