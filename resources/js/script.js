b7314e80941a4133afb68420cb253fa109d7db18
Nate Hicks <nhicksdesign@gmail.com>
2017-07-24 22:12:29 -0400

    Loader Extended

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     setTimeout(function () {
         $('body').addClass('loaded');
         $('h1').css('color', '#222222');
-    }, 3000);
+    }, 5000);
 
     /*STICKY NAVIGATION*/
     $("#about").waypoint(function (direction) {
